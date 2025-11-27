8609e3396083a82e0eb91a987105bfd9009b645b
Wen Wei Tseng <sosiristseng@gmail.com>
2025-11-27 20:53:26 +0800

    rm version info

--- docs/pythonplot-lit.jl
+++ docs/pythonplot-lit.jl
@@ -9,11 +9,3 @@ Random.seed!(2022)
 plt.figure()
 plt.plot(1:5, rand(1:6, 5))
 plt.gcf()
-
-# ## Runtime information
-import Pkg
-Pkg.status()
-
-#---
-import InteractiveUtils
-InteractiveUtils.versioninfo()
